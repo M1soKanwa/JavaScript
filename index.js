5ad78fd4f11bc5b8a8f8bf4938350201d9ce5874
Roman Novikov <chickenramen2211@gmail.com>
2024-02-20 18:10:55 +0100

    *new* function

---
+++ index.js
@@ -0,0 +1,12 @@
+function Dog(name, color, age) {
+    this.name = name;
+    this.color = color;
+    this.age = age;
+}
+
+const dog1 = new Dog('Tuzik', 'black', '1');
+
+const dog2 = new Dog('Knopa', 'white', '3');
+
+console.log(Dog);
+
