1f7399fc2e29a009e6af3232f373f6f23cca1865
Roman Novikov <chickenramen2211@gmail.com>
2024-02-20 19:49:21 +0100

    Home work About DOG

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ function Dog(name, color, age) {
     this.name = name;
     this.color = color;
     this.age = age;
+    this.gav = function() {
+        return this.name + 'gav-gav-gav';
+    }
 }
 
 const dog1 = new Dog('Tuzik', 'black', '1');
 
 const dog2 = new Dog('Knopa', 'white', '3');
+
+const dog3 = new Dog('Saimon', 'skyblue', '7');
+
