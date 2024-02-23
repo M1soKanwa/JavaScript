4c4520684bb3776b36cdda83a006bf92b0090538
Roman Novikov <chickenramen2211@gmail.com>
2024-02-23 13:05:01 +0100

    Computed Property

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-// Задача 
-
-// У нас есть функция
-function greetingUser(user) {
-    return `Hey ${user.name} ${user.surname} You will be ${user.age + 1} next year.`;
-}
-
-// также у нас есть объект 
 const user = {
-    name: 'John',
-    surname: 'Doe',
-    age: 19,
+    'name': 'John', // Ключ 'name', значение - 'John'
+    'favorite color': 'red' ,
+    0: 'value'
 }
 
-//Написать функцию greetingUser так, чтобы она приветствовала пользователя и указывала, сколько ему будет лет в следующем году.
+// Ключом в объекте может быть рядок или тип данных Symbol
+ 
+//  .  --->>> оператор доступа по имени свойства (когда речь идет про валидные идентификаторы)
 
-// Пример вызывания функции 
-greetingUser(user); // Hey John Doe, u will be 20 next year.
+console.log(user.name); // John
+// user.'favorite color'; --->>>  syntax ERROR
+// user.0; --->>> synax ERROR
 
+// [] - оператор доступа к вычислительным свойствам (когда речь идёт про не валидные идентификаторы)
+
+console.log(user['favorite color']); // RED
+
+console.log(user[0]); // Value
+
+
+const someOption = {
+    2: 10
+}
+
+// Задача : получить значение которое находится под ключом 2.
+
+console.log(someOption[2]);
+console.log(someOption ['2']);
 
