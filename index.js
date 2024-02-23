23590e48c3533f6313f55dc66b105e210056587f
Roman Novikov <chickenramen2211@gmail.com>
2024-02-23 14:59:25 +0100

    Practise *for in*

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ object - объект,свойства которого мы хотим пере
 for (let key in user) {
     console.log(`${key} ---> ${user[key]}`); // key ---> value(свойства).
 }
+
+// Задача : Написать функцию , которая принимает объект и выводит все значения и свойства, оформления *...*
+// А ключи - "..."
+
+const obj =  {
+    key1: 'Y.U',
+    key2: 'F.I',
+    key3: 'S.T',
+    key4: 'N.A'
+}
+
+function styling(){
+
+for (const key in obj) {
+    console.log(`"${key}" have *${obj[key]}*`)
+    }
+}    
