b9b57acfb581a6af76278c844a036173fa29d7d9
Roman Novikov <chickenramen2211@gmail.com>
2024-02-23 14:23:07 +0100

    Example *in* operator

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const obj = {
-    Mon: 'get a drive license',
-    Tue: 'come to new boss',
-    Wed: 'visit parents',
-    Thu: 'go to gym',
-    Fri: 'study spanish language',
-    Sat: 'go to swim',
-    Sun: 'meet with friends',
-    'favorite color' : 'skyblue', // practise operator IN
-    0: 'gaga' // practise operator IN
+function greeting(user) {
+    return `Hello ${'name' in user ? user.name : 'Anonym'}`;
 }
 
-// оператор in 
-// in - бинарный оператор
-// "key" in obj(название объекта).
-
-console.log('Mon' in obj);
-console.log('Tue' in obj);
-console.log('Wed' in obj);
-console.log('Thu' in obj);
-console.log('Fri' in obj);
-console.log('Sat' in obj);
-console.log('Sun' in obj);
-
-console.log('favorite color' in obj); // practise operator IN
-console.log(0 in obj); // practise operator IN
-
-// Оператор In может проверять только одно!
+const user = {
+    name: 'Tommy',
+    email: 'chokopai29023@gmail.com'
+}
