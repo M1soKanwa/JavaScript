c8a1a5e1a4bce73c33491eca11a6fd8243221a01
Roman <151219635+M1soKanwa@users.noreply.github.com>
2024-03-17 02:44:34 +0100

    Date

--- index.js
+++ index.js
@@ -1,69 +1,53 @@
-//            Стрелочные функции    ( => )              //
+///////                       DATE                      ///////
 
 
+//        let now = new Date();
+//        console.log(now);
 
-//function sum(a,b) {
-//    return a + b
-// }
+// Так можно вывести настоящую дату(время)  //
 
-// console.log(sum(821, 9));
+//        let date = new Date(24 * 3600 * 1000);  // (часы секунды милисекунды)
+//        console.log(date);
 
-//let sum = function() {
-//    return a + b
-//}
+//        let date = new Date("2020-04-14 00:01:05");  // Можно вводить любую дату
+//        let date = new Date("3.3.26");
+//        console.log(date);
 
-// console.log(sum(821, 9));
+//        let date = new Date(2012, 2, 4, 33, 3, 1);    -->> (33) ушло вперед,добавил день.
+//        console.log(date);
 
-
-
-//    Вариант 1 
-
-// let sum = (a, b) => {
-//     return a + b
-// }
-
-// console.log(sum(821, 9));
+//        let date = new Date("2020-04-14 00:01:05");  
+//        date.setFullYear(2022);
+//        date.setMonth(14);   // добавился год,потому как 14 месяцев .     Месяца считаются от нуля -- 0 это январь.
+//        date.setDate(15);  // меняется день.
+//        console.log(date);
 
 
+// let date = new Date("2017-02-26");
+// console.log(date.getFullYear());
+// console.log(date.getMonth() +1);
+// console.log(date.getDate());
 
-//    Вариант 2
+function getDate(date) {     // Функция которая выводит адекватное и красивое время
 
-//  let sum = (a, b) => a + b;
+  let result = ''
 
-//  console.log(sum(821, 9));
+  if (date.getDate() < 10) {
+      result = result + '0'
+  }
 
+  result = result + date.getDate() + '.'
 
-//                       Если стрелочная функция не имеет аргументов,то можно поступить так...
+  if (date.getMonth() < 9) {
+      result = result + '0'
+  }
+  result = result + (date.getMonth() + 1) + '.'
 
+  result = result + date.getFullYear()
+  
+  return result
 
-// let random = () => Math.round(Math.random() * 100)
+}
 
-// console.log(random());
-
-//                     Что если у функции 1 аргумент?         //
-
-
-// let random = (a) => Math.round(Math.random() * a);
-
-let random = a => Math.round(Math.random() * a);
-
-console.log(random(50));
-
-
-//       Задача 
-
-//              Вариант 1 
-
-//    let count = 0;
-//    let action = function() {
-//        count++
-//        console.log('Повторение ' + count);
-//     } 
-
-
-//  setInterval(action, 400);
-
-//                 Вариант 2
-
-   let count = 0;
-   setInterval(() => console.log(`Номер  ${++count}`), 1000);
+let date = new Date("2017-02-10");
+console.log(getDate(date));
