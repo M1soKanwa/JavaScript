f7935dbba6bf42f5c8cf446f278d3465a6fbce77
Roman <151219635+M1soKanwa@users.noreply.github.com>
2024-04-01 18:43:20 +0200

    Create JSON

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 45,
+    footSize: 45
+}
 
+// stringify - делает JS объект в JSON. 
 
+// parse - делает JSON в JS объект.
 
-console.log('Request Data...');
-
-
-
-
-
-const p = new Promise(function(resolve, reject) {
-       setTimeout(() => {
-        console.log('Preparing data...');
-        const backendData = {
-            server :'aws', 
-            port : 2000,
-            status : 'working'
-        }
-        resolve(backendData)
-       },2000)   
-})
-
-p.then(data => {
-    return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        data.modified = true
-        resolve(data)
-       // console.log('Data Received', backendData);
-    },2000)
-    })
-}).then(clientData => {
-    console.log('Data received', clientData);
-})
+const jsonString = JSON.stringify(user);
