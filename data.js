4b9648f86abe06823990fde7d62fe8349a76b0dc
Roman <151219635+M1soKanwa@users.noreply.github.com>
2024-04-01 21:09:21 +0200

    Create user card

---
+++ data.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+        id: 1,
+        name: 'John',
+        description: 'Description for John',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }, {
+        id: 2,
+        name: 'Josh',
+        description: 'Description for Josh',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }, {
+        id: 3,
+        name: 'Anna',
+        description: 'Description for Anna',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }, {
+        id: 4,
+        name: 'Antony',
+        description: 'Description for Antony',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }, {
+        id: 5,
+        name: 'Alex',
+        description: 'Description for Alex',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }, {
+        id: 6,
+        name: 'Bixsi',
+        description: 'Description for Bixsi',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }, {
+        id: 7,
+        name: 'Alexa',
+        description: 'Description for Alexa',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }, {
+        id: 8,
+        name: 'Tom',
+        description: 'Description for Tom',
+        profilePicture: 'https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg'
+    }
+    ];
--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 45,
-    footSize: 45
+/*
+
+<article class="card-wrapper">
+            <img class="card-image" src="https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg" alt="John avatar">
+            <h2 class="username">John</h2>
+            <p class="description">Description for John</p>
+        </article>
+
+*/
+
+
+const root = document.querySelector('#root');
+
+function createUserCart (user) {
+    const article = document.createElement('article');
+    article.classList.add('card-wrapper');
+    const img = document.createElement('img');
+    
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('card-image');
+
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
+    img.classList.add('username');
+
+    const p = document.createElement('p');
+    p.append(user.description);
+    p.classList.add('description');
+
+    article.append(img, h2, p);
+
+    return article;
 }
 
-// stringify - делает JS объект в JSON. 
+const cardArray = data.map(user => createUserCart(user));
 
-// parse - делает JSON в JS объект.
+root.append(...cardArray);
 
-const jsonString = JSON.stringify(user);
+
