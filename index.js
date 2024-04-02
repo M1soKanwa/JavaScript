9eb7fcd44fc7f9b7a58591e4182194db04d18142
Roman <151219635+M1soKanwa@users.noreply.github.com>
2024-04-02 15:54:23 +0200

    Async Await

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
-/*
+// async 
+// await
 
-<article class="card-wrapper">
-            <img class="card-image" src="https://www.goha.ru/s/f/NUk4/plaN44/L6NX3iFBmN.jpg" alt="John avatar">
-            <h2 class="username">John</h2>
-            <p class="description">Description for John</p>
-        </article>
+const URL = 'https://randomuser.me/api'
 
-*/
+function loadDataWithoutAsync(url) {
+    const p = fetch(url);
 
+    const jsonResponse = p.then((response) => {
+        return response.json();
+    });
 
-const root = document.querySelector('#root');
-
-function createUserCart (user) {
-    const article = document.createElement('article');
-    article.classList.add('card-wrapper');
-    const img = document.createElement('img');
-    
-    img.setAttribute('src', user.profilePicture);
-    img.setAttribute('alt', user.name);
-    img.classList.add('card-image');
-
-    const h2 = document.createElement('h2');
-    h2.append(user.name);
-    img.classList.add('username');
-
-    const p = document.createElement('p');
-    p.append(user.description);
-    p.classList.add('description');
-
-    article.append(img, h2, p);
-
-    return article;
+    jsonResponse.then((data) => {
+        console.log(data);
+    });
 }
 
-const cardArray = data.map(user => createUserCart(user));
+// loadDataWithoutAsync(URL);
 
-root.append(...cardArray);
+// ASYNC / AWAIT 
 
+async function loadDataAsync(url) {
+    const p = fetch(url);
+    const response = await p;
+    const data = await response.json();
 
+    console.log(data);
+}
+
+loadDataAsync(URL); 
